608bc56ad73a525ac7d41c93479838be65c1a22b
SayanDeveloper <samantasayan23@gmail.com>
2023-04-02 20:42:42 +0530

    🎉 initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default App
