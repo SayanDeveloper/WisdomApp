bc82f471097542769042d1d99c0ec1db13f790c8
SayanDeveloper <samantasayan23@gmail.com>
2023-04-03 03:40:15 +0530

    🔥 cleanup

--- App.tsx
+++ App.tsx
@@ -11,25 +11,6 @@ const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
 
-  const [cardsData, setCardsData] = useState([])
-
-  const videoListFetcher = async () => {
-    try {
-      const response = await fetch(
-        'https://www.wisdomapp.in/api/v1/content/?page=1&limit=10',
-      );
-      const json = await response.json();
-      console.log(json.results[0])
-      setCardsData(json.results)
-      return json.movies;
-    } catch (error) {
-      console.error(error);
-    }  
-  }
-
-  useEffect(() => {
-    videoListFetcher()
-  }, [])
   return (
     <NavigationContainer>
       <Stack.Navigator>
